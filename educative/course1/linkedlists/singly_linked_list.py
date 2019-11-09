5fcd0cc1f1079bb7d912f77f1c72916a82a4fd89
Rohit Arora <rohit.arora.sindhwani@gmail.com>
2019-11-09 15:20:23 -0500

    educative: linked lists - singly linked list - implemented insert_at_end()

--- educative/course1/linkedlists/singly_linked_list.py
+++ educative/course1/linkedlists/singly_linked_list.py
@@ -24,6 +24,21 @@ class SinglyLinkedList:
 
         self.head = new
 
+    def insert_at_end(self, data):
+        if self.is_empty():
+            self.insert_at_head(data)
+            return
+
+        new = Node()
+        new.value = data
+        new.next = None
+
+        last = self.head
+        while last.next is not None:
+            last = last.next
+
+        last.next = new
+
 
 def main():
     example = SinglyLinkedList()
