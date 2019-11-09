560f390b7700ff86faddc80399eebe6a577d9fd7
Rohit Arora <rohit.arora.sindhwani@gmail.com>
2019-11-09 15:15:24 -0500

    educative: linked lists - singly linked list - class definition

---
+++ educative/course1/linkedlists/singly_linked_list.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self):
+        self.value = None
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+
+def main():
+    example = SinglyLinkedList()
+
+
+if __name__ == "__main__":
+    main()
