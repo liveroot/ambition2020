5d9ca66c4174c70bb0f564742d5544b9d73953b6
Rohit Arora <rohit.arora.sindhwani@gmail.com>
2019-11-09 15:18:34 -0500

    educative: linked lists - singly linked list - implemented is_empty()

--- educative/course1/linkedlists/singly_linked_list.py
+++ educative/course1/linkedlists/singly_linked_list.py
@@ -8,6 +8,10 @@ class SinglyLinkedList:
     def __init__(self, head=None):
         self.head = head
 
+    def is_empty(self):
+        if self.head is None:
+            return True
+
 
 def main():
     example = SinglyLinkedList()
