6cf677780900b3515b22d79e26079ae71ad62eed
Rohit Arora <rohit.arora.sindhwani@gmail.com>
2019-11-10 17:53:18 -0500

    educative: linked lists - doubly linked list - implemented insert functions

--- educative/course1/linkedlists/doubly_linked_list.py
+++ educative/course1/linkedlists/doubly_linked_list.py
@@ -12,3 +12,55 @@ class DoublyLinkedList:
     def is_empty(self):
         if self.head is None:
             return True
+
+    def insert_at_head(self, data):
+        if self.is_empty():
+            self.head = Node()
+            self.head.value = data
+            return
+
+        new = Node()
+        new.value = data
+        new.prev = None
+        new.next = self.head
+
+        if self.head is not None:
+            self.head.prev = new
+
+        self.head = new
+
+    def insert_at_end(self, data):
+        if self.is_empty():
+            self.insert_at_head(data)
+            return
+
+        new = Node()
+        new.value = data
+        new.next = None
+
+        current = self.head
+        while current.next is not None:
+            current = current.next
+
+        new.prev = current
+        current.next = new
+
+    def insert_after(self, after, data):
+        if self.is_empty():
+            print("list is empty")
+            return None
+
+        new = Node()
+        new.value = data
+
+        current = self.head
+        while current.next is not None and current.value is not after:
+            current = current.next
+
+        if current is None:
+            print("value not found")
+            return
+
+        new.prev = current
+        new.next = current.next
+        current.next = new
