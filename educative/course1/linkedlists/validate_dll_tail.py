75c45d679fe5dff11d6bc2e63c465f9380917fb7
Rohit Arora <rohit.arora.sindhwani@gmail.com>
2019-11-24 16:29:22 -0500

    educative: linked lists - doubly linked list with tail - adding validation module for dll w/ tail

---
+++ educative/course1/linkedlists/validate_dll_tail.py
@@ -0,0 +1,54 @@
+import educative.course1.linkedlists.doubly_linked_list_tail as dll_tail
+
+
+def main():
+    example = dll_tail.DoublyLinkedListTail()
+
+    # inserting at head
+    print("inserting at head...")
+    example.insert_at_head(5)
+    example.insert_at_head(6)
+    example.insert_at_head(7)
+    example.insert_at_head(8)
+
+    # inserting at end
+    print("inserting at end...")
+    example.insert_at_end(10)
+    example.insert_at_end(9)
+
+    # inserting after a node
+    print("inserting after...")
+    example.insert_after(6, 12)
+    example.insert_after(9, 20)
+    example.insert_after(7, 14)
+    example.insert_after(20, 15)
+    example.insert_after(21, 18)
+
+    # printing list
+    example.print_list()
+
+    # deleting at head
+    print("deleting at head...")
+    example.delete_at_head()
+    example.print_list()
+
+    # deleting by value
+    print("deleting by value...")
+    example.delete_by_value(12)
+    example.print_list()
+
+    # deleting by value
+    print("deleting at end...")
+    example.delete_at_end()
+    example.print_list()
+
+    # searching in linked list
+    print("searching node...")
+    if example.search_node(21):
+        print("node is found")
+    else:
+        print("node not found")
+
+
+if __name__ == '__main__':
+    main()
