b028e4d739d27e13ba1f1a027b1c5548d5dbe280
Rohit Arora <rohit.arora.sindhwani@gmail.com>
2019-12-18 16:40:36 -0500

    educative: stacks_queues - implementing stack - editing validation module

--- educative/course1/stacks_queues/validate_stack.py
+++ educative/course1/stacks_queues/validate_stack.py
@@ -2,27 +2,64 @@ import educative.course1.stacks_queues.stack as stack
 
 
 def main():
+    print("Stack initialized with capacity = 5")
     example = stack.Stack(5)
 
-    # pushing elements to stack
-    print("pushing to stack")
+    print("------")
+    print("pushing to stack...")
     example.push(5)
     example.push(6)
     example.push(7)
     example.push(8)
     example.push(9)
     example.push(2)
+    example.push(7)
+
+    print()
     example.print_stack()
 
-    # popping elements from stack
-    print("popping data..")
-    print(example.pop())
+    print("------")
+    print("peeking at the top...")
+    print(example.peek())
+
+    print("------")
+    print("popping from stack..")
+    example.pop()
+    example.pop()
+
+    print()
     example.print_stack()
 
-    # peeking from the stack
-    print("peeking at the top..")
-    print(str(example.peek()))
+    print("------")
+    print("peeking at the top...")
+    print(example.peek())
+
+    print("------")
+    print("popping from stack..")
+    example.pop()
+    example.pop()
+    example.pop()
+    example.pop()
+    example.pop()
+
+    print()
     example.print_stack()
+
+    print("------")
+    print("peeking at the top...")
+    print(example.peek())
+
+    print("------")
+    print("pushing to stack...")
+    example.push(21)
+    example.push(22)
+
+    print()
+    example.print_stack()
+
+    print("------")
+    print("peeking at the top...")
+    print(example.peek())
 
 
 if __name__ == '__main__':
