b60ad91fca738ec073554ac90ce666a46f2b4eef
Rohit Arora <rohit.arora.sindhwani@gmail.com>
2019-12-18 17:16:14 -0500

    educative: stacks_queues - challenge 1

---
+++ educative/course1/stacks_queues/ch1_generate_binary_numbers.py
@@ -0,0 +1,39 @@
+import educative.course1.stacks_queues.queue as q
+
+input_data = 3
+expected_output_data = [1, 10, 11]
+
+
+def generate_binary_numbers(input_data):
+    result = []
+    n = input_data
+
+    # initialize queue with capactiy  n + 1, because
+    # we are enqueing 2 values for each dequeue()
+    queue = q.Queue(n + 1)
+
+    # initialize queue with first value to start the conversion
+    queue.enqueue(1)
+
+    # for each entry:
+    # 1. dequeue from the list and add to result array
+    # 2. append 0 and enqueue it back
+    # 3. append 1 and enqueue it back
+    # this process enqueues binary form of each entry to the result array
+    for i in range(n):
+        temp = queue.dequeue()
+        result.append(temp)
+        queue.enqueue(int(str(temp) + "0"))
+        queue.enqueue(int(str(temp) + "1"))
+
+    return result
+
+
+def main():
+    print("Input: " + str(input_data))
+    print("Expected: " + str(expected_output_data))
+    print("Output: " + str(generate_binary_numbers(input_data)))
+
+
+if __name__ == '__main__':
+    main()
