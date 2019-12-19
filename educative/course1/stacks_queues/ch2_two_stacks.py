ff98a63fa38abff50c01f692b92df1093bb51d4c
Rohit Arora <rohit.arora.sindhwani@gmail.com>
2019-12-19 01:24:06 -0500

    educative: stacks_queues - challenge 2

---
+++ educative/course1/stacks_queues/ch2_two_stacks.py
@@ -0,0 +1,88 @@
+class TwoStacks():
+    def __init__(self, capacity=None):
+        self.capacity = capacity
+        self.elements = [None] * self.capacity
+        self.top1 = -1
+        self.top2 = self.capacity
+
+    def is_full(self):
+        if abs(self.top1 - self.top2) is 1:
+            return True
+        return False
+
+    def is_empty(self):
+        if self.top1 is -1 and self.top2 is self.capacity:
+            return True
+        return False
+
+    def push1(self, data):
+        if self.is_full():
+            print("can't push anymore, stack full...")
+            return None
+
+        if self.top1 < self.top2 - 1:
+            self.top1 += 1
+            self.elements[self.top1] = data
+            print("stack1 - pushed data = " + str(data))
+        return None
+
+    def push2(self, data):
+        if self.is_full():
+            print("can't push anymore, stack full...")
+            return None
+
+        if self.top2 > self.top1 + 1:
+            self.top2 -= 1
+            self.elements[self.top2] = data
+            print("stack2 - pushed data = " + str(data))
+        return None
+
+    def pop1(self):
+        if self.is_empty():
+            print("can't pop, stack empty...")
+            return None
+
+        if self.top1 > -1:
+            result = self.elements[self.top1]
+            self.elements[self.top1] = None
+            self.top1 -= 1
+            print("stack1 - popped data = " + str(result))
+            return result
+        return None
+
+    def pop2(self):
+        if self.is_empty():
+            print("can't pop, stack empty...")
+            return None
+
+        if self.top2 < self.capacity:
+            result = self.elements[self.top2]
+            self.elements[self.top2] = None
+            self.top2 += 1
+            print("stack2 - popped data = " + str(result))
+            return result
+        return None
+
+    def peek1(self):
+        if self.top1 is -1:
+            return None
+        return str(self.elements[self.top1])
+
+    def peek2(self):
+        if self.top2 is self.capacity:
+            return None
+        return str(self.elements[self.top2])
+
+    def print_stack(self):
+        pretty_list = self.prettify()
+        print(pretty_list)
+
+        return None
+
+    def prettify(self):
+        printer = ""
+        for i in range(self.capacity):
+            printer += str(i) + " -> [  " + str(self.elements[i]) + "  ]"
+            if i is not self.capacity: printer += "\n"
+
+        return printer
---
+++ educative/course1/stacks_queues/validate_two_stacks.py
@@ -0,0 +1,86 @@
+import educative.course1.stacks_queues.ch2_two_stacks as two_stacks
+
+
+def main():
+    print("Stack initialized with capacity = 5")
+    example = two_stacks.TwoStacks(5)
+
+    print("------")
+    print("pushing to stack 1...")
+    example.push1(5)
+    example.push1(6)
+    example.push1(7)
+    print("pushing to stack 2...")
+    example.push2(8)
+    example.push2(9)
+    example.push2(2)
+    example.push2(7)
+
+    print()
+    print("peeking from stack 1...")
+    print(example.peek1())
+
+    print()
+    print("peeking from stack 2...")
+    print(example.peek2())
+
+    print()
+    example.print_stack()
+
+    print("------")
+    print("popping from stack 1..")
+    example.pop1()
+    print("popping from stack 2..")
+    example.pop2()
+
+    print()
+    print("peeking from stack 1...")
+    print(example.peek1())
+
+    print()
+    print("peeking from stack 2...")
+    print(example.peek2())
+
+    print()
+    example.print_stack()
+
+    print("------")
+    print("popping from stack 1..")
+    example.pop1()
+    example.pop1()
+    print("popping from stack 2..")
+    example.pop2()
+    example.pop2()
+    example.pop2()
+
+    print()
+    print("peeking from stack 1...")
+    print(example.peek1())
+
+    print()
+    print("peeking from stack 2...")
+    print(example.peek2())
+
+    print()
+    example.print_stack()
+
+    print("------")
+    print("pushing to stack 2...")
+    example.push2(21)
+    example.push2(22)
+    example.push2(23)
+
+    print()
+    print("peeking from stack 1...")
+    print(example.peek1())
+
+    print()
+    print("peeking from stack 2...")
+    print(example.peek2())
+
+    print()
+    example.print_stack()
+
+
+if __name__ == '__main__':
+    main()
